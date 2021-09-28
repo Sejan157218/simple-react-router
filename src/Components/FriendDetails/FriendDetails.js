2a8fcf082b263f11883445b2cac0615faf808075
sejan <sejan157218@gmail.com>
2021-09-29 03:18:38 +0600

    show results in dynamic url

--- src/Components/FriendDetails/FriendDetails.js
+++ src/Components/FriendDetails/FriendDetails.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import './FriendDetails.css'
 
 const FriendDetails = () => {
+    const {friendID} =useParams()
+    const url = `https://jsonplaceholder.typicode.com/users/${friendID}`
+    const [friend,setFriend]=useState({})
+   
+    useEffect(()=>{
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>setFriend(data))
+    },[]);
+    const {name,email,website,phone,address} =friend;
     return (
-        <div>
-           <h1> comming soon</h1>
+        <div className="frienddetails-container">
+            <h1>I am : {name}</h1>
+            <p>Call me : {phone}</p>
+            <p>Email me : {email}</p>
+            <p>Visit me : {website}</p>
+            <p>I live In : {address?.street}</p>
         </div>
     );
 };
