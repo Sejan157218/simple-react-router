2807e134cc5da5f6d69447a98ea3329c35d0db0a
sejan <sejan157218@gmail.com>
2021-09-29 04:02:46 +0600

    see dybamic url in button onclick

--- src/Components/Friend/Friend.js
+++ src/Components/Friend/Friend.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {useHistory, NavLink } from 'react-router-dom';
 import './Friend.css'
 
 const Friend = (props) => {
     const {name,email,website,phone,address,id} = props.friend;
 
+    const history = useHistory()
+    const handlerFriendsClick=()=>{
+        history.push(`/friend/${id}`)
+    }
     return (
         <div className="friend-container">
             <h1>I am : {name}</h1>
@@ -12,7 +16,10 @@ const Friend = (props) => {
             <p>Email me : {email}</p>
             <p>Visit me : {website}</p>
             <p>I live In : {address.street}</p>
-            <NavLink to={`/friend/${id}`} >Visite Me</NavLink>
+            <NavLink to={`/friend/${id}`} target="_blank">Visite Me</NavLink>
+            <NavLink to={`/friend/${id}`} target="_blank"><button>Visite Me</button></NavLink>
+            <button onClick={handlerFriendsClick} target="_blank">Visit me Button</button>
+
         </div>
     );
 };
--- src/Components/FriendDetails/FriendDetails.js
+++ src/Components/FriendDetails/FriendDetails.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 import './FriendDetails.css'
 
 const FriendDetails = () => {
-    const {friendID} =useParams()
+    const {friendID} =useParams();
+
     const url = `https://jsonplaceholder.typicode.com/users/${friendID}`
     const [friend,setFriend]=useState({})
-   
+   const history = useHistory()
     useEffect(()=>{
         fetch(url)
         .then(res=>res.json())
         .then(data=>setFriend(data))
     },[]);
     const {name,email,website,phone,address} =friend;
+
+    const handlerAllFriends=()=>{
+        history.push('/friends')
+    }
     return (
         <div className="frienddetails-container">
             <h1>I am : {name}</h1>
@@ -20,6 +25,7 @@ const FriendDetails = () => {
             <p>Email me : {email}</p>
             <p>Visit me : {website}</p>
             <p>I live In : {address?.street}</p>
+            <button onClick={handlerAllFriends}>see all friend</button>
         </div>
     );
 };
